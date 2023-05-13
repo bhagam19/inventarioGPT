1fdb8dc608a516988d904c7c6778a685d72b9584
Adolfo Ruiz <bhagam19@gmail.com>
2023-05-13 15:02:53 -0500

    config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Configuración de la base de datos
+define('DB_HOST', 'localhost'); // Host de la base de datos
+define('DB_USER', 'nombre_usuario'); // Nombre de usuario de la base de datos
+define('DB_PASSWORD', 'contraseña'); // Contraseña de la base de datos
+define('DB_NAME', 'nombre_base_datos'); // Nombre de la base de datos
+
+?>
